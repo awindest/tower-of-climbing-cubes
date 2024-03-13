d5841f94c68305c2ce20a86276936fa0cf0ab4e0
Bill Indest <56596420+awindest@users.noreply.github.com>
2024-03-12 20:20:22 -0400

    test of css/sccs

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 // These two lines are from sveltekit-markdown-blog project:
 // import adapter from '@sveltejs/adapter-vercel'
 // import { vitePreprocess } from '@sveltejs/kit/vite'
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'; // this is for vite 5
 
 /** @type {import('@sveltejs/kit').Config} */
